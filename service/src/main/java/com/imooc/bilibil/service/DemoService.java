fda2fd8926dea88af129ec13558953300370a4f6
ToAotao <xiaotaowu345@gmail.com>
2023-02-26 00:52:41 -0800

    2/26/2023 created demo project

---
+++ service/src/main/java/com/imooc/bilibil/service/DemoService.java
@@ -0,0 +1,18 @@
+package com.imooc.bilibil.service;
+
+import com.imooc.bilibili.dao.DemoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class DemoService {
+    @Autowired
+    private DemoDao demoDao;
+
+    public Map<String, Object> query(long id) {
+        return demoDao.query(id);
+    }
+
+}
